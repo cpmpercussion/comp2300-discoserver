053ca2dfc1913bac201ff91515aa15d6b2080849
benjamingray <bengray178@gmail.com>
2020-03-09 14:21:47 +1100

    add version flag

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,21 @@ mod server;
 use server::GdbServer;
 
 fn main() {
+    if get_version_from_argv() {
+        println!("disco-emulator v{}.{}.{}", 1, 1, 0);
+        return;
+    }
+
     println!("started emulator server");
     GdbServer::start_server();
+}
+
+fn get_version_from_argv() -> bool {
+    let mut args = std::env::args();
+    while let Some(arg) = args.next() {
+        if arg == "-v" || arg == "--version" {
+            return true;
+        }
+    }
+    return false;
 }
